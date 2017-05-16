4099c2f844adee1e3aff60c63685d163cc24c1b6
JihwanKim <jihwan6589@naver.com>
2017-05-16 22:02:56 +0900

    add get file part and reduce dependence at MainActivity

--- AndroidBluetoothWithBluecovePrac/app/src/main/java/com/example/jihwa/androidbluetoothwithbluecoveprac/MainActivity.java
+++ AndroidBluetoothWithBluecovePrac/app/src/main/java/com/example/jihwa/androidbluetoothwithbluecoveprac/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.jihwa.androidbluetoothwithbluecoveprac;
 
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothServerSocket;
-import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,8 +15,6 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.io.IOException;
-import java.util.UUID;
 
 /*
  * 본 예제에서 activity_main.xml 은 분석하지않고 그냥 복붙함
@@ -40,15 +36,11 @@ public class MainActivity extends AppCompatActivity {
     static BluetoothAdapter mBluetoothAdapter;
     private ArrayAdapter<String> mConversationArrayAdapter;
 
-    static boolean isConnectionError = false;
-    private static final String TAG = "BluetoothServer";
-    private final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+    private static final String TAG = "MainActivity";
 
-    private BluetoothServerSocket bluetoothServerSocket;
-    private String mConnectedDeviceName;
     private SocketManager socketManager;
 
-    private MsgTask mMsgTask;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,9 +53,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String sendMessage = mInputEditText.getText().toString();
-                if(sendMessage.length()>0 && socketManager!=null) {
+                if(sendMessage.length()>0 && socketManager!=null && !socketManager.isReceiveFile()) {
                     Log.d(TAG,"SEND MSG :" +sendMessage);
-                    sendMessage(sendMessage);
+                    socketManager.sendMessage(sendMessage);
                     mInputEditText.setText(" ");
                 }
             }
@@ -74,117 +66,27 @@ public class MainActivity extends AppCompatActivity {
 
         mConversationArrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_activated_1);
         mMessageListView.setAdapter(mConversationArrayAdapter);
+        context = getApplicationContext();
+        callSocketManager();
+    }
 
+    private void callSocketManager(){
+        // bluetooth adapter 초기화
+        // 만약, off면, on으로 바꾸고, SocketManager실행
         Log.d(TAG,"Initializing Bluetooth adapter..");
-
-        // 로컬어뎁터를 반환한다.
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if(mBluetoothAdapter == null){
-            showErrorDialog("This device is not implement Bluetooth.");
-            return;
-        }
-
         if(!mBluetoothAdapter.isEnabled()){
             Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(intent,REQUEST_BLUETOOTH_ENABLE);
-        }else{
-            Log.d(TAG,"Initialisation successful.");
-
-            // 2. 페어링 되어 있는 블루투스 장치들의 목록을 보여줌
-            // 3. 목록에서 블루투스 장치를 선택하면 선택한 디바이스를
-            // 인자로 하여 doConnect 함수가 호출.
-            try {
-                bluetoothServerSocket = mBluetoothAdapter.listenUsingRfcommWithServiceRecord("SDP NAME",MY_UUID);
-            } catch (IOException e) {
-                Log.e(TAG,"cannot create server socket");
-            }
-
-            // 이렇게 안하면, 대기상태에서 계속유지되어, UI가 초기화되지 않음.
-            Thread thread = new Thread() {
-                @Override
-                public void run() {
-                    while(true) {
-                        try {
-                            BluetoothSocket socket = bluetoothServerSocket.accept();
-
-                            socketManager = new SocketManager(socket);
-                            mConnectedDeviceName = socketManager.getName();
-                            Log.d(TAG, "accept success. new connectTask . device = " + mConnectedDeviceName);
-
-                            mMsgTask = new MsgTask(socketManager);
-                            mMsgTask.execute();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            };
-            thread.start();
+        }else {
+            socketManager = new SocketManager(mConnectionStatus, mConversationArrayAdapter,context);
         }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mMsgTask.cancel(true);
-    }
-
-    private void sendMessage(String sendMessage) {
-        mConversationArrayAdapter.insert("YOU" + ": " + sendMessage, 0);
-        socketManager.sendMsg(sendMessage);
-    }
-
-
-    private class MsgTask extends  AsyncTask<Void,String,Boolean>{
-        SocketManager socketManager = null;
-        public MsgTask(SocketManager socketManager) {
-            this.socketManager = socketManager;
-        }
-
-        @Override
-        protected void onProgressUpdate(String... values) {
-            mConnectionStatus.setText("Connected to " + mConnectedDeviceName);
-            Log.d(TAG,"GET MSG = " + values[0]);
-            mConversationArrayAdapter.insert(mConnectedDeviceName + ": " + values[0], 0);
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            while(true){
-                if(!socketManager.isRunning()) return false;
-                if(socketManager.getMsgSize()>0) {
-                    String str = socketManager.getTake();
-                    publishProgress(str);
-                }
-            }
-        }
-
-        @Override
-        protected void onPostExecute(Boolean aBoolean) {
-            mMsgTask.cancel(true);
-            mMsgTask = null;
-            socketManager.close();
-            socketManager = null;
-        }
-    }
-
-
-    private void showErrorDialog(String message) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Quit");
-        builder.setCancelable(false);
-        builder.setMessage(message);
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-                if(isConnectionError){
-                    isConnectionError = false;
-                    finish();
-                }
-            }
-        });
-        builder.create().show();
+        socketManager.onCancel();
     }
 
     private void showQuitDialog(String message) {
@@ -202,13 +104,13 @@ public class MainActivity extends AppCompatActivity {
         builder.create().show();
     }
 
-
     // 블루투스 실행시키는 결과값을 확인하여, showPairedDevicesListDialog를 호출하거나,
     // 블루투스가 존재하지 않는 경우, 종료 다이얼로그를 보여준다.
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode == REQUEST_BLUETOOTH_ENABLE){
             if(resultCode == RESULT_OK){
+                socketManager = new SocketManager(mConnectionStatus,mConversationArrayAdapter,context);
             }
             if(resultCode == RESULT_CANCELED){
                 showQuitDialog("You need to enable bluetooth");
--- AndroidBluetoothWithBluecovePrac/app/src/main/java/com/example/jihwa/androidbluetoothwithbluecoveprac/SocketManager.java
+++ AndroidBluetoothWithBluecovePrac/app/src/main/java/com/example/jihwa/androidbluetoothwithbluecoveprac/SocketManager.java
@@ -1,14 +1,27 @@
 package com.example.jihwa.androidbluetoothwithbluecoveprac;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-import org.jetbrains.annotations.NotNull;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Date;
+import java.util.UUID;
 import java.util.concurrent.ArrayBlockingQueue;
+
 
 
 /**
@@ -17,111 +30,162 @@ import java.util.concurrent.ArrayBlockingQueue;
 
 public class SocketManager {
     private static final String TAG = "BluetoothServer";
+    private final BluetoothAdapter mLocalDevice;
+    private final Context mContext;
+    private BluetoothServerSocket mBluetoothServerSocket;
     private BluetoothSocket mBluetoothSocket = null;
+    private String mConnectedDeviceName;
     private InputStream mInputStream = null;
     private OutputStream mOutputStream = null;
     private ArrayBlockingQueue<String> mMsg = new ArrayBlockingQueue<>(20);
 
+    private final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 
-    private boolean isRunning = false;
+    private TextView mConnectionStatus;
+    private ArrayAdapter<String> mConversationArrayAdapter;
 
-    public SocketManager(@NotNull BluetoothSocket socket){
-        mBluetoothSocket = socket;
-        isRunning = true;
+    private MsgTask mMsgTask;
+
+    private boolean isReceiveFile = false;
+    private File file = null;
+    private FileOutputStream mFileOutputStream;
+
+
+    public SocketManager(TextView textViewConnectionStatus, ArrayAdapter<String> conversationArrayAdapter, Context context){
+        mContext = context;
+        mConnectionStatus = textViewConnectionStatus;
+        mConversationArrayAdapter = conversationArrayAdapter;
+        // 로컬어뎁터를 반환한다.
+        mLocalDevice = BluetoothAdapter.getDefaultAdapter();
+        if(mLocalDevice == null){
+            //showErrorDialog("This device is not implement Bluetooth.");
+            return;
+        }
+        Log.d(TAG,"Initialisation successful.");
+
+        // 2. 페어링 되어 있는 블루투스 장치들의 목록을 보여줌
+        // 3. 목록에서 블루투스 장치를 선택하면 선택한 디바이스를
+        // 인자로 하여 doConnect 함수가 호출.
         try {
-            mInputStream = mBluetoothSocket.getInputStream();
-            mOutputStream = mBluetoothSocket.getOutputStream();
+            mBluetoothServerSocket = mLocalDevice.listenUsingRfcommWithServiceRecord("SDP NAME",MY_UUID);
         } catch (IOException e) {
-            Log.d(TAG,"socket closed " + e.getMessage());
+            Log.e(TAG,"cannot create server mBluetoothSocket");
         }
 
-        Thread receiveThread = new Thread(){
+        // 이렇게 안하면, 대기상태에서 계속유지되어, UI가 초기화되지 않음.
+        Thread thread = new Thread() {
             @Override
             public void run() {
-                byte[] readBuffer = new byte[1024];
-                int readBufferPosition = 0;
-                while(true){
-                    try{
-                        if(!mBluetoothSocket.isConnected()) {
-                            isRunning = false;
-                            return;
-                        }
-                        // 근데 왜또되냐 진짜 미치겠네.
-                        // 읽을 수 있는 bytes 를 확인함
-                        // 아래는 주석 두줄은 구글번역기
-                        // 블록하지 않고이 입력 Stream로부터 읽어 들일 수가있는 (또는 스킵 할 수있다)
-                        // 바이트 수의 추정치. 입력 Stream의 마지막에 이르렀을 때의 바이트 수.
-                        int bytesAvailable = mInputStream.available();
-                        // 만약, 대기중인게 있을 경우,
-                        if(bytesAvailable>0){
-                            // packet으로 온 bytes를 저장할 변수를 선언함
-                            byte[] packetBytes = new byte[bytesAvailable];
-                            //read from the inputStream
-                            // mInputStream에서 읽어온 bytes를 packetBytes에 저장함.
-                            mInputStream.read(packetBytes);
+                try {
+                    Log.d(TAG, "waiting for new paired device");
+                    mBluetoothSocket = mBluetoothServerSocket.accept();
+                    mConnectedDeviceName = mBluetoothSocket.getRemoteDevice().getName();
+                    Log.d(TAG, "accept success. new connectTask . device = " + mConnectedDeviceName);
 
-                            // 추정된 바이트의 길이보다 작을동안 실행.
-                            for(int i = 0 ; i<bytesAvailable; i++){
-                                // b를 packetBytes 의 i번째 byte로 초기화시킴.
-                                byte b = packetBytes[i];
-                                // 만약, 엔터 이스케이프 시퀸스가 발견될 경우,
-                                // 읽어들인 readBufferPosition의 크기를 지닌 encodedBytes를 선언하는데,
-                                // 해당 변수는 결과적으로 readBuffer에 저장되어있는 모든 배열을 카피해감.
-                                // TODO : check . why arraycopy readBuffer to encodedBytes
-                                if(b == '\n'){
-                                    byte[] encodedBytes = new byte[readBufferPosition];
-                                    System.arraycopy(readBuffer,0,encodedBytes,0,encodedBytes.length);
-                                    // encodedBytes에 저장되어있는 값을 UTF-8 인코딩으로 recvMessage에
-                                    // 저장함.
-                                    String recvMessage = new String(encodedBytes,"UTF-8");
+                    mMsgTask = new MsgTask();
+                    mMsgTask.execute();
 
-                                    // readBufferPosition에 0을 대입함.
-                                    readBufferPosition = 0;
-
-                                    Log.d(TAG,"ReceiveMsg = "+recvMessage);
-                                    mMsg.add(recvMessage);
-                                }else{
-                                    // 그렇지 않으면,
-                                    // readBuffer에 readBufferPostion의 위치에 b를 저장함.
-                                    readBuffer[readBufferPosition++] = b;
-                                }
-                            }
-                        }
+                    try {
+                        mInputStream = mBluetoothSocket.getInputStream();
+                        mOutputStream = mBluetoothSocket.getOutputStream();
                     } catch (IOException e) {
-                        Log.d(TAG,"disconnected",e);
-                        return;
+                        Log.d(TAG,"mBluetoothSocket closed " + e.getMessage());
                     }
+                    msgReceive();
+                } catch (IOException e) {
+                    Log.d(TAG,"socket cannot accept!");
                 }
             }
         };
-        receiveThread.start();
+        thread.start();
+
     }
-    public void sendMsg(String str){
-        str+="\n";
+
+    //
+    private void msgReceive() {
+        // message send
+        byte[] readBuffer = new byte[1024];
+        int readBufferPosition = 0;
+        while(true){
+            try{
+                if(!mBluetoothSocket.isConnected()) {
+                    return;
+                }
+                // 근데 왜또되냐 진짜 미치겠네.
+                // 읽을 수 있는 bytes 를 확인함
+                // 아래는 주석 두줄은 구글번역기
+                // 블록하지 않고이 입력 Stream로부터 읽어 들일 수가있는 (또는 스킵 할 수있다)
+                // 바이트 수의 추정치. 입력 Stream의 마지막에 이르렀을 때의 바이트 수.
+                int bytesAvailable = mInputStream.available();
+                // 만약, 대기중인게 있을 경우,
+                if(bytesAvailable>0) {
+                    // packet으로 온 bytes를 저장할 변수를 선언함
+                    byte[] packetBytes = new byte[bytesAvailable];
+                    //read from the inputStream
+                    // mInputStream에서 읽어온 bytes를 packetBytes에 저장함.
+                    mInputStream.read(packetBytes);
+                    if (isReceiveFile) {
+                        //int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.);
+
+                        int c;
+                        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+                        file = new File(path,"test.txt");
+
+                        FileOutputStream out = new FileOutputStream(file);//mContext.openFileOutput("test.txt",Context.MODE_PRIVATE);
+                        out.write(packetBytes);
+                        out.flush();
+                        out.close();
+                        Log.d(TAG,"file write[" + file.getPath() + "] Name[" + file.getName() + "] fileSize = " + file.length());
+                        isReceiveFile = false;
+                    }else{
+
+                    // 추정된 바이트의 길이보다 작을동안 실행.
+                    for (int i = 0; i < bytesAvailable; i++) {
+                        // b를 packetBytes 의 i번째 byte로 초기화시킴.
+                        byte b = packetBytes[i];
+                        // 만약, 엔터 이스케이프 시퀸스가 발견될 경우,
+                        // 읽어들인 readBufferPosition의 크기를 지닌 encodedBytes를 선언하는데,
+                        // 해당 변수는 결과적으로 readBuffer에 저장되어있는 모든 배열을 카피해감.
+                        // TODO : check . why arraycopy readBuffer to encodedBytes
+                        if (b == '\n') {
+                            byte[] encodedBytes = new byte[readBufferPosition];
+                            System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                            // encodedBytes에 저장되어있는 값을 UTF-8 인코딩으로 recvMessage에
+                            // 저장함.
+                            String recvMessage = new String(encodedBytes, "UTF-8");
+
+
+                            // readBufferPosition에 0을 대입함.
+                            readBufferPosition = 0;
+
+                            if (recvMessage.equals("sendFile")) {
+                                isReceiveFile = true;
+                            }
+                            mMsg.add(recvMessage);
+                        } else {
+                            // 그렇지 않으면,
+                            // readBuffer에 readBufferPostion의 위치에 b를 저장함.
+                            readBuffer[readBufferPosition++] = b;
+                        }
+                    }
+                }
+                }
+            } catch (IOException e) {
+                Log.d(TAG,"disconnected",e);
+                return;
+            }
+        }
+    }
+
+    public void sendMessage(String sendMessage) {
+        mConversationArrayAdapter.insert("YOU" + ": " + sendMessage, 0);
+        sendMessage+="\n";
         try {
-            mOutputStream.write(str.getBytes());
+            mOutputStream.write(sendMessage.getBytes());
             mOutputStream.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-    }
-
-    public BluetoothSocket getBluetoothSocket() {
-        return mBluetoothSocket;
-    }
-
-    public String getName() {
-        return mBluetoothSocket.getRemoteDevice().getName();
-    }
-
-    public String getTake(){
-        try {
-            return mMsg.take();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 
     public int getMsgSize() {
@@ -136,11 +200,91 @@ public class SocketManager {
         try {
             mBluetoothSocket.close();
         } catch (IOException e) {
-            Log.d(TAG,"socket closed");
+            Log.d(TAG,"mBluetoothSocket closed");
         }
     }
 
-    public boolean isRunning() {
-        return isRunning;
+    // message 처리부분
+    private class MsgTask extends AsyncTask<Void,String,Void> {
+        public MsgTask() {
+        }
+
+        // Message를 인자로 받아서 UI를 업데이트시켜주는 부분.
+        // 만약, 파일이올경우 다른행동을함.
+        @Override
+        protected void onProgressUpdate(String... values) {
+            mConnectionStatus.setText("Connected to " + mConnectedDeviceName);
+            Log.d(TAG, "GET MSG = " + values[0]);
+            mConversationArrayAdapter.insert(mConnectedDeviceName + ": " + values[0], 0);
+
+        }
+
+        // 계속해서 메세지가 왔는지 여부를 체크해서, UI 업데이트를 콜하는 부분
+        @Override
+        protected Void doInBackground(Void... params) {
+            while(mBluetoothSocket.isConnected()){
+                if(getMsgSize()>0) {
+                    String str = null;
+                    try {
+                        str = mMsg.take();
+                    } catch (InterruptedException e) {
+                        Log.e(TAG,"interrupt exception in msg take - " + e.getMessage());
+                    }
+                    if(str!=null)
+                        publishProgress(str);
+                }
+            }
+            return null;
+        }
+
+        // doInBackground가 종료될 경우 호출하여, task를 cancel하는 부분
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            cancel(true);
+            close();
+        }
+
+        @Override
+        protected void onCancelled(Void aVoid) {
+            super.onCancelled(aVoid);
+            Log.d(TAG,"task cancel check");
+        }
+    }
+
+    public boolean isReceiveFile(){
+        return isReceiveFile;
+    }
+
+    private void receiveFile(byte [] bytes){
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+        file = new File(path + "/"+new Date()+".txt");
+        Log.d(TAG,"path = " + file.getPath());
+        try {
+            mFileOutputStream = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Log.d(TAG,"bytes = " + bytes.length);
+            byte [] byte2 = "123213123".getBytes();
+            mFileOutputStream.write(byte2);
+
+            mFileOutputStream.flush();
+            mFileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        file = null;
+        mFileOutputStream = null;
+
+        isReceiveFile = false;
+    }
+    public void onCancel(){
+        try {
+            mBluetoothSocket.close();
+        } catch (IOException e) {
+        }
+        if(!mMsgTask.isCancelled())
+            mMsgTask.cancel(true);
     }
 }
